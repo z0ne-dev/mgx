d90d2c48b72e81175aa1bbd51e6aeed40fbe0316
Ch. König <me@kurabloodlust.eu>
2023-02-17 10:39:52 +0100

    ci test

---
+++ migrator_test.go
@@ -0,0 +1,39 @@
+package mgx_test
+
+import (
+	"context"
+	_ "embed"
+	"github.com/z0ne-dev/mgx"
+	"testing"
+)
+
+var migrations = []mgx.Migration{
+	mgx.NewRawMigration("raw migration: single query, create table foo", "CREATE TABLE foo (id INT PRIMARY KEY)"),
+	mgx.NewRawMigration("raw migration: multi query, create table bar, alter table bar", "CREATE TABLE bar (id INT PRIMARY KEY); ALTER TABLE bar ADD COLUMN name VARCHAR(255)"),
+	mgx.NewMigration("fn migration", func(ctx context.Context, cmd mgx.Commands) error {
+		if _, err := cmd.Exec(ctx, "CREATE TABLE foobar (id INT PRIMARY KEY)"); err != nil {
+			return err
+		}
+		if _, err := cmd.Exec(ctx, "INSERT INTO foobar (id) VALUES (1)"); err != nil {
+			return err
+		}
+		return nil
+	}),
+}
+
+func TestMigrate(t *testing.T) {
+	// create db connection
+	// TODO: replace with your db connection
+
+	// create migrator
+	migrator, err := mgx.New(mgx.Migrations(migrations...))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// run migrator
+	err := migrator.Migrate(context.Background(), db)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
